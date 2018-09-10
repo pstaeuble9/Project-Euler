616e5e69bf504992c4b160c0994a768de5502968
Patrick Staeuble <35965091+pstaeuble9@users.noreply.github.com>
2018-09-10 12:53:39 -0400

    Add files via upload

---
+++ Euler1PS.js
@@ -0,0 +1,13 @@
+let sum = 0;
+
+function findTheSum(max) {
+    for (let i = 3; i < max; i += 1) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+
+console.log(findTheSum(1000));
